3534db40430b61108d030073f20a92ffc7907069
krispikris <kris@kristopherhan.com>
2023-12-05 10:54:48 -0800

    dark mode working on refresh

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ const settingsMenuToggle = () => {
 darkButton.onclick = () => {
   darkButton.classList.toggle('dark-btn-on');
   document.body.classList.toggle('dark-theme');
+
+  if (localStorage.getItem('theme') == 'light') {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
 };
 
 if (localStorage.getItem('theme') == 'light') {
   darkButton.classList.remove('dark-btn-on');
   document.body.classList.remove('dark-theme');
-} else if (localStorage.getItem('theme') == 'light') {
+} else if (localStorage.getItem('theme') == 'dark') {
   darkButton.classList.add('dark-btn-on');
   document.body.classList.add('dark-theme');
 } else {
